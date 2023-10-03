5972ff68bf72f3240ec1801bddfbf51c8bcb1777
YordanCode <145832388+YordanCode@users.noreply.github.com>
2023-10-03 18:47:29 +0300

    Basic Algorithms solutions

---
+++ BasicAlgorithms/Kadane'sAlgorithm/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+static int FindMaxSubarraySum(int[] arr)
+{
+    int n = arr.Length;
+    int maxSum = int.MinValue;
+    int currentSum = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        currentSum = Math.Max(arr[i], currentSum + arr[i]);
+        maxSum = Math.Max(maxSum, currentSum);
+    }
+    return maxSum;
+}
+
+int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+int maxSum = FindMaxSubarraySum(arr);
+Console.WriteLine("Maximum subarray sum: " + maxSum);
